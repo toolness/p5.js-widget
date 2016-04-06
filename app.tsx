7992bd2280e5c0d5f78eb7dc349377d0a4788d7d
Atul Varma <varmaa@gmail.com>
2016-04-06 19:13:12 -0400

    Make a simple App component.

--- app.tsx
+++ app.tsx
@@ -6,14 +6,39 @@ import ReactDOM = require("react-dom");
 import React = require("react");
 import CodeMirror = require("codemirror");
 
-let i = 0;
+interface AppProps {
+}
 
-setInterval(() => {
-  ReactDOM.render(
-    <p>hello from react {i++}</p>,
-    document.getElementById('app')
-  );
-}, 300);
+interface AppState {
+  counter: number
+}
+
+class App extends React.Component<AppProps, AppState> {
+  _interval: number
+
+  constructor(props) {
+    super(props);
+    this.state = { counter: 0 };
+  }
+
+  componentDidMount() {
+    this._interval = setInterval(() => {
+      this.setState((prevState, prevProps) => {
+        return { counter: prevState.counter + 1 };
+      });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this._interval);
+  }
+
+  render() {
+    return <p>Hello from react {this.state.counter}</p>;
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'));
 
 var cm = CodeMirror(document.getElementById('codemirror'), {
   value: 'hello from codemirror'
