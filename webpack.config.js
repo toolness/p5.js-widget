9c2e9613f6f4c82116dc756baf9fa05856c3066c
Atul Varma <varmaa@gmail.com>
2016-04-07 05:58:37 -0400

    use source maps for css.

--- webpack.config.js
+++ webpack.config.js
@@ -7,14 +7,14 @@ module.exports = {
     // Add `.ts` and `.tsx` as a resolvable extension.
     extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
   },
-  devtool: 'eval',
+  devtool: 'source-map',
   module: {
     loaders: [
       // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
       { test: /\.tsx?$/, loader: 'ts-loader' },
       {
         test: /\.scss$/,
-        loaders: ["style", "css", "sass"]
+        loaders: ["style", "css?sourceMap", "sass?sourceMap"]
       }
     ]
   }
