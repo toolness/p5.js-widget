e96a0a8517cd858d6b2e3758a70d6901a6791253
Atul Varma <varmaa@gmail.com>
2016-04-08 17:59:36 -0400

    document data-height, add data-preview-width. fixes #10.

--- lib/main.tsx
+++ lib/main.tsx
@@ -7,16 +7,23 @@ import App from "./app";
 
 let defaultSketchJS = require("raw!./default-sketch.js") as string;
 
+const DEFAULT_PREVIEW_WIDTH = 150;
+
 require("../css/style.css");
 
 function start() {
   let qs = url.parse(window.location.search, true).query;
   let autoplay = (qs['autoplay'] === 'on');
   let initialContent = qs['sketch'] || defaultSketchJS;
+  let previewWidth = parseInt(qs['previewWidth']);
+
+  if (isNaN(previewWidth)) {
+    previewWidth = DEFAULT_PREVIEW_WIDTH;
+  }
 
   ReactDOM.render(
     <App initialContent={initialContent.trim()}
-         previewWidth={150}
+         previewWidth={previewWidth}
          autoplay={autoplay} />,
     document.getElementById('app-holder')
   );
