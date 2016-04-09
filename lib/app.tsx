723f6fd4ba1d9faf0939ad2344c25cabc507d66e
Atul Varma <varmaa@gmail.com>
2016-04-09 17:07:12 -0400

    Add play/stop icons for #14.

--- lib/app.tsx
+++ lib/app.tsx
@@ -34,6 +34,19 @@ let ErrorMessage = (props: ErrorMessage) => (
   </div>
 );
 
+// https://useiconic.com/open/
+let OpenIconicMediaPlay = () => (
+  <svg className="media-play" width="8" height="8" viewBox="0 0 8 8">
+    <path d="M0 0v6l6-3-6-3z" transform="translate(1 1)" />
+  </svg>
+);
+
+let OpenIconicMediaStop = () => (
+  <svg className="media-stop" width="8" height="8" viewBox="0 0 8 8">
+    <path d="M0 0v6h6v-6h-6z" transform="translate(1 1)" />
+  </svg>
+);
+
 export default class App extends React.Component<AppProps, AppState> {
   constructor(props) {
     super(props);
@@ -96,9 +109,15 @@ export default class App extends React.Component<AppProps, AppState> {
           <a className="p5-logo" href="http://p5js.org/" target="_blank">
             <img src="static/img/p5js-beta.svg" alt="p5js.org"/>
           </a>
-          <button onClick={this.handlePlayClick}>Play</button>
+          <button onClick={this.handlePlayClick}>
+            <OpenIconicMediaPlay/>
+            Play
+          </button>
           {this.state.isPlaying
-            ? <button onClick={this.handleStopClick}>Stop</button>
+            ? <button onClick={this.handleStopClick}>
+                <OpenIconicMediaStop/>
+                Stop
+              </button>
             : null }
           {this.state.editorContent !== this.props.initialContent
             ? <button onClick={this.handleRevertClick}>Revert</button>
