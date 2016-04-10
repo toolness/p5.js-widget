bbcd88848b20c338f4582671f5d935a3c594da35
Atul Varma <varmaa@gmail.com>
2016-04-10 01:23:15 -0400

    Oops, forgot to add these in last commit.

--- lib/app.tsx
+++ lib/app.tsx
@@ -1,5 +1,6 @@
 import React = require("react");
 
+import Toolbar from "./toolbar";
 import Editor from "./editor";
 import Preview from "./preview";
 
@@ -32,19 +33,6 @@ let ErrorMessage = (props: ErrorMessage) => (
     <span className="error-message-line">Line {props.line}:</span>
     {" " + props.message}
   </div>
-);
-
-// https://useiconic.com/open/
-let OpenIconicMediaPlay = () => (
-  <svg className="media-play" width="8" height="8" viewBox="0 0 8 8">
-    <path d="M0 0v6l6-3-6-3z" transform="translate(1 1)" />
-  </svg>
-);
-
-let OpenIconicMediaStop = () => (
-  <svg className="media-stop" width="8" height="8" viewBox="0 0 8 8">
-    <path d="M0 0v6h6v-6h-6z" transform="translate(1 1)" />
-  </svg>
 );
 
 export default class App extends React.Component<AppProps, AppState> {
@@ -97,6 +85,7 @@ export default class App extends React.Component<AppProps, AppState> {
 
   render() {
     let errorLine = null;
+    let canRevert = (this.state.editorContent !== this.props.initialContent);
 
     if (this.state.lastError &&
         this.state.editorContent === this.state.previewContent) {
@@ -105,24 +94,10 @@ export default class App extends React.Component<AppProps, AppState> {
 
     return (
       <div className="app">
-        <div className="toolbar">
-          <a className="p5-logo" href="http://p5js.org/" target="_blank">
-            <img src="static/img/p5js-beta.svg" alt="p5js.org"/>
-          </a>
-          <button onClick={this.handlePlayClick}>
-            <OpenIconicMediaPlay/>
-            Play
-          </button>
-          {this.state.isPlaying
-            ? <button onClick={this.handleStopClick}>
-                <OpenIconicMediaStop/>
-                Stop
-              </button>
-            : null }
-          {this.state.editorContent !== this.props.initialContent
-            ? <button onClick={this.handleRevertClick}>Revert</button>
-            : null}
-        </div>
+        <Toolbar
+         onPlayClick={this.handlePlayClick}
+         onStopClick={this.state.isPlaying && this.handleStopClick}
+         onRevertClick={canRevert && this.handleRevertClick} />
         <div className="panes">
           <Editor content={this.state.editorContent}
                   errorLine={errorLine}
