f28b882a0c17bf67f30ddacaf5aae572050b0e15
Atul Varma <varmaa@gmail.com>
2016-04-12 09:23:52 -0400

    update update-gh-pages.sh.

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -4,16 +4,21 @@ set -eu
 
 echo "Regenerating bundle."
 
-rm -rf bundle.*
+rm -rf *.bundle.*
 npm run bundle
 
 echo "Copying/moving files in working dir."
 
-rm -rf __temp_bundle.js __temp_bundle.js.map __temp_static \
+rm -rf \
+  __temp_main.bundle.js __temp_main.bundle.js.map \
+  __temp_preview-frame.bundle.js __temp_preview-frame.bundle.js.map \
+  __temp_static \
   __temp_p5-widget.html __temp_p5-widget.js __temp_preview-frame.html \
   __temp_index.html
-mv bundle.js __temp_bundle.js
-mv bundle.js.map __temp_bundle.js.map
+mv main.bundle.js __temp_main.bundle.js
+mv main.bundle.js.map __temp_main.bundle.js.map
+mv preview-frame.js __temp_preview-frame.bundle.js
+mv preview-frame.bundle.js.map __temp_preview-frame.bundle.js.map
 cp -r static/ __temp_static/
 cp p5-widget.html __temp_p5-widget.html
 cp p5-widget.js __temp_p5-widget.js
@@ -24,10 +29,15 @@ echo "Switching to gh-pages branch."
 
 git checkout gh-pages
 
-rm -rf bundle.js bundle.js.map static p5-widget.html p5-widget.js \
+rm -rf \
+  main.bundle.js main.bundle.js.map \
+  preview-frame.bundle.js preview-frame.bundle.js.map \
+  static p5-widget.html p5-widget.js \
   preview-frame.html index.html
-mv __temp_bundle.js bundle.js
-mv __temp_bundle.js.map bundle.js.map
+mv __temp_main.bundle.js main.bundle.js
+mv __temp_main.bundle.js.map main.bundle.js.map
+mv __temp_preview-frame.bundle.js preview-frame.bundle.js
+mv __temp_preview-frame.bundle.js.map preview-frame.bundle.js.map
 mv __temp_static static
 mv __temp_p5-widget.html p5-widget.html
 mv __temp_p5-widget.js p5-widget.js
@@ -36,7 +46,10 @@ mv __temp_index.html index.html
 
 echo "Staging new/changed files."
 
-git add bundle.js bundle.js.map static/ p5-widget.html p5-widget.js \
+git add \
+  main.bundle.js main.bundle.js.map \
+  preview-frame.bundle.js preview-frame.bundle.js.map \
+  static/ p5-widget.html p5-widget.js \
   preview-frame.html index.html
 
 #echo "Done. Run 'git diff --staged' to review changes."
