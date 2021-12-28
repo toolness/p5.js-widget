8ca7bcfbfbd67541bf7278bd59976c63a13728dd
Ben Stolovitz <ben@stolovitz.com>
2021-12-27 23:09:44 -0500

    move stuff and fix query params

--- lib/main.tsx
+++ lib/main.tsx
@@ -13,14 +13,14 @@ require("../css/p5-widget-codemirror-theme.css");
 
 function start() {
   let embeddingPageURL = document.referrer;
-  let qs = new URL(window.location.search).searchParams;
-  let id = embeddingPageURL + '_' + qs['id'];
-  let baseSketchURL = qs['baseSketchURL'] || embeddingPageURL;
-  let autoplay = (qs['autoplay'] === 'on');
-  let initialContent = qs['sketch'] || defaultSketchJS;
-  let p5version = qs['p5version'] || defaults.P5_VERSION;
-  let previewWidth = parseInt(qs['previewWidth']);
-  let maxRunTime = parseInt(qs['maxRunTime'])
+  let qs = new URLSearchParams(window.location.search);
+  let id = embeddingPageURL + '_' + qs.get('id');
+  let baseSketchURL = qs.get('baseSketchURL') || embeddingPageURL;
+  let autoplay = (qs.get('autoplay') === 'on');
+  let initialContent = qs.get('sketch') || defaultSketchJS;
+  let p5version = qs.get('p5version') || defaults.P5_VERSION;
+  let previewWidth = parseInt(qs.get('previewWidth'));
+  let maxRunTime = parseInt(qs.get('maxRunTime'))
   if (isNaN(previewWidth)) {
     previewWidth = defaults.PREVIEW_WIDTH;
   }
